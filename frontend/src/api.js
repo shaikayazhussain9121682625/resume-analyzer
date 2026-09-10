be8422f0c3023358c0f7ecd270e4bf40d0da8753
shaikayazhussain9121682625 <shaikayazhussain46@gmail.com>
2026-09-10 10:32:58 +0530

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
-﻿import axios from "axios";
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ayazhussain.pythonanywhere.com",
   withCredentials: true,
 });
 
